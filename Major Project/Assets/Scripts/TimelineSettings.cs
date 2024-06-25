763b5e27cc175379f888159121660b25d7b7c3f3
HatAYE <h.alchaban@student.saedubai.com>
2024-06-26 01:10:34 +0400

    timeline fixed, just add TimelineSettings to the lvl 1 intro object

---
+++ Major Project/Assets/Scripts/TimelineSettings.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineSettings : MonoBehaviour
+{
+    PlayableDirector director;
+    [SerializeField] KeyCode skipKey= KeyCode.Space;
+    void Start()
+    {
+        director = GetComponent<PlayableDirector>();
+        director.stopped += Deactivate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (director.state == PlayState.Playing)
+        {
+            GameManager.instance.ChangeState(gameStates.frozen);
+        }
+        if (Input.GetKeyDown(skipKey))
+        {
+            director.time = director.duration;
+            director.Evaluate();
+        }
+    }
+
+    void Deactivate(PlayableDirector director)
+    {
+        GameManager.instance.ChangeState(gameStates.playing);
+        gameObject.SetActive(false);
+    }
+}
