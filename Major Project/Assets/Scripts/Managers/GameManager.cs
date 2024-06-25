7198d3cd62a4b03114a8eaa68d9c70306d5ebd62
HatAYE <h.alchaban@student.saedubai.com>
2024-06-26 01:49:48 +0400

    Revert "Merge remote-tracking branch 'origin/Armaan' into Hayat"
    This reverts commit 32b2ecfa00b15ea89e9519f6ca7dfb6124b2cda2, reversing
    changes made to 4db5a8a4680d9c3bfae2b9244764fd71dd18a993.

--- Major Project/Assets/Scripts/Managers/GameManager.cs
+++ Major Project/Assets/Scripts/Managers/GameManager.cs
@@ -70,7 +70,6 @@ public class GameManager : MonoBehaviour
         {
             AssignButtons();
             pauseMenu.SetActive(false);
-            print("not active");
         }
         ChangeState(gameStates.playing);
         //currentLevel = SceneManager.GetActiveScene().buildIndex+1;
@@ -107,15 +106,15 @@ public class GameManager : MonoBehaviour
         resumeButton = GameObject.Find("resume").GetComponent<Button>();
         restartButton= GameObject.Find("Restart level").GetComponent<Button>();
         exitButton = GameObject.Find("Exit").GetComponent<Button>();
-        //restartCheckPointButton = GameObject.Find("Restart from last checkpoint").GetComponent<Button>();
-        //audioSettingsButton= GameObject.Find("Audio settings").GetComponent<Button>();
+        restartCheckPointButton = GameObject.Find("Restart from last checkpoint").GetComponent<Button>();
+        audioSettingsButton= GameObject.Find("Audio settings").GetComponent<Button>();
         //audioBackButton= GameObject.Find("Back audio button").GetComponent<Button>();
 
         resumeButton.onClick.AddListener(TogglePause);
         restartButton.onClick.AddListener(RestartLevel);
         exitButton.onClick.AddListener(ExitGame);
-        //restartCheckPointButton.onClick.AddListener(pl.Respawn);
-        //audioSettingsButton.onClick.AddListener(OpenAudioMenu);
+        restartCheckPointButton.onClick.AddListener(pl.Respawn);
+        audioSettingsButton.onClick.AddListener(OpenAudioMenu);
         //audioBackButton.onClick.AddListener(ExitAudioMenu);
     }
     public void TogglePause()
