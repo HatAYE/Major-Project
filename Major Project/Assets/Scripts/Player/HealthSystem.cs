5670b764859cbc28c54a1475b54b8109417a2539
HatAYE <h.alchaban@student.saedubai.com>
2024-07-08 23:50:52 +0400

    bug fixes

--- Major Project/Assets/Scripts/Player/HealthSystem.cs
+++ Major Project/Assets/Scripts/Player/HealthSystem.cs
@@ -26,10 +26,10 @@ public class HealthSystem : MonoBehaviour
             currentHealth = hp;
         }
 
-        /*if(currentHealth<= 0)
+        if(currentHealth<= 0 && hp>=1)
         {
             GetComponent<Controls>().Respawn();
-        }*/
+        }
 
         for (int i = 0; i < HPUI.Length; i++)
         {
