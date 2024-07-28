f63f06acea14dee0f4392a329d00eac156da64e3
HassanAbusalih <hassan.abusalih@student.saedubai.com>
2024-07-28 14:46:04 +0400

    animated dialogue

--- Major Project/Assets/Scripts/Dialogue/DialogueUI.cs
+++ Major Project/Assets/Scripts/Dialogue/DialogueUI.cs
@@ -1,5 +1,6 @@
 using Conversa.Runtime.Events;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -14,6 +15,7 @@ public class DialogueUI : MonoBehaviour
     [SerializeField] TextMeshProUGUI dialogueLine;
     [SerializeField] Button nextLineButton;
     [SerializeField] GameObject choiceOptionButtonPrefab;
+    [SerializeField] float timePerWord = 0.1f;
     public bool inDialogue => dialogueWindow.activeSelf;
     public static DialogueUI Instance { get; private set; }
     Action skipDialogueAction;
@@ -45,12 +47,26 @@ public class DialogueUI : MonoBehaviour
         UpdateGameState();
         UpdateImage(avatar);
         characterName.text = actor;
-        dialogueLine.text = message;
-
+        Coroutine lineAnimation = StartCoroutine(AnimateLine(message, onContinue));
         nextLineButton.enabled = true;
+        
+
+        Action displayFullLine = null;
+        displayFullLine += () =>
+        {
+            StopCoroutine(lineAnimation);
+            SetTextTransparency(dialogueLine, 255);
+            UpdateSkipAction(onContinue);
+        };
+
+        UpdateSkipAction(displayFullLine);
+    }
+
+    private void UpdateSkipAction(Action skipAction)
+    {
         nextLineButton.onClick.RemoveAllListeners();
-        nextLineButton.onClick.AddListener(() => onContinue());
-        skipDialogueAction = onContinue;
+        nextLineButton.onClick.AddListener(() => skipAction());
+        skipDialogueAction = skipAction;
     }
 
     void UpdateGameState()
@@ -97,5 +113,61 @@ public class DialogueUI : MonoBehaviour
         characterImage.sprite = null;
         characterImage.gameObject.SetActive(false);
         UpdateGameState();
+    }
+
+    // Instead of adding to the text character by character, this makes the entire text transparent then fades it in one word at a time.
+    IEnumerator AnimateLine(string line, Action onContinue)
+    {
+        dialogueLine.text = line;
+        dialogueLine.ForceMeshUpdate();
+
+        SetTextTransparency(dialogueLine);
+
+        string[] words = line.Split(' ');
+        int wordStartIndex = 0;
+        int wordEndIndex = 0;
+        for (int wordIndex = 0; wordIndex < words.Length; wordIndex++)
+        {
+            wordEndIndex += words[wordIndex].Length;
+            float time = 0;
+
+            while (time < timePerWord)
+            {
+                time += Time.deltaTime;
+                byte alpha = (byte)(Mathf.Lerp(0, 1, time / timePerWord) * 255);
+                SetTextTransparency(dialogueLine, alpha, wordStartIndex, wordEndIndex);
+                yield return null;
+            }
+
+            wordStartIndex = wordEndIndex + 1;
+            wordEndIndex += 1;
+        }
+
+        UpdateSkipAction(onContinue);
+    }
+
+    public void SetTextTransparency(TextMeshProUGUI dialogueLine, byte alpha = 0, int startIndex = 0, int endIndex = int.MaxValue)
+    {
+        TMP_TextInfo textInfo = dialogueLine.textInfo;
+        endIndex = Mathf.Min(endIndex, textInfo.characterCount - 1);
+
+        for (int i = startIndex; i <= endIndex; i++)
+        {
+            if (!textInfo.characterInfo[i].isVisible) continue;
+
+            int materialIndex = textInfo.characterInfo[i].materialReferenceIndex;
+            Color32[] newVertexColors = textInfo.meshInfo[materialIndex].colors32;
+            int vertexIndex = textInfo.characterInfo[i].vertexIndex;
+
+            Color32 color = newVertexColors[vertexIndex];
+            color.a = alpha;
+
+            newVertexColors[vertexIndex + 0] = color;
+            newVertexColors[vertexIndex + 1] = color;
+            newVertexColors[vertexIndex + 2] = color;
+            newVertexColors[vertexIndex + 3] = color;
+        }
+
+        dialogueLine.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
     }
 }
