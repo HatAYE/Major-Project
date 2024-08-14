1f930ace308fd16aee9b39d8b0ebf9b557b9838b
HatAYE <h.alchaban@student.saedubai.com>
2024-08-14 15:11:37 +0400

    jumping enemy animation

--- Major Project/Assets/Scripts/Player/Controls.cs
+++ Major Project/Assets/Scripts/Player/Controls.cs
@@ -121,7 +121,7 @@ public class Controls : MonoBehaviour
             essenceCollected = 4;
         }
         collectedSparepart = false;
-        if (Input.GetKeyDown(KeyCode.V)) transform.position = new Vector3(-29.3f, -9.064f, 0);
+        if (Input.GetKeyDown(KeyCode.V)) transform.position = new Vector3(-77.8f, 3.5f, 0);
 
         if (Input.GetKeyDown(KeyCode.B)) transform.position = new Vector3(72.2f, -3.6f, 0);
     }
